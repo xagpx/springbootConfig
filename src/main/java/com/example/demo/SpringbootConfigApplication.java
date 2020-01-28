9cf7bbcd1a511b597d80cfc1118cf0a88aa4d4c9
zgh <zgh@windows10.microdone.cn>
2020-01-28 15:20:38 +0800

    test

---
+++ src/main/java/com/example/demo/SpringbootConfigApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringbootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigApplication.class, args);
+	}
+}
